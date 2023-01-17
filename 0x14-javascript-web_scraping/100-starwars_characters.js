e0488dede07933fc3249abd3c596874e67dc6d8b
Raheem Salifu Kasim <r.kasim@aol.com>
2023-01-17 23:14:11 +0000

    Task Update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (err, response, content) {
-  if (!err) {
-    const characters = JSON.parse(content).characters;
-    characters.forEach((character) => {
-      request(character, function (err, response, content) {
-        if (!err) {
-          console.log(JSON.parse(content).name);
+const BASE_URL = 'https://swapi-api.hbtn.io/api';
+
+if (process.argv.length > 2) {
+  request(`${BASE_URL}/films/${process.argv[2]}/`, (err, res, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+
+    charactersURL.forEach(element => {
+      request(element, (err, res, body) => {
+        if (err) {
+          console.log(err);
         }
+        console.log(JSON.parse(body).name);
       });
     });
-  }
-});
+  });
+}
